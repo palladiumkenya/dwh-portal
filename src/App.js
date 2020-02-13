dda394e59fd6eb5e0d6e2081f658587fe937beeb
Gitonga Kithinji <gitonga.kithinji@thepalladiumgroup.com>
2020-02-13 15:57:38 +0300

    initial commit for National data warehouse re-design
    Signed-off-by: Gitonga Kithinji <Gitonga.Kithinji@thepalladiumgroup.com>

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { HashRouter, Route, Switch } from "react-router-dom";
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const loading = () => <div className="animated fadeIn pt-3 text-center">Loading...</div>;
+
+// Containers
+const DefaultLayout = React.lazy(() => import('./containers/DefaultLayout'));
+
+// Pages
+const Login = React.lazy(() => import('./views/Pages/Login'));
+
+class App extends Component{
+    render() {
+        return (
+            <HashRouter>
+                <React.Suspense fallback={loading()}>
+                    <Switch>
+                        <Route exact path="/login" name="Login Page" render={props => <Login {...props}/>} />
+                        <Route path="/" name="Home" render={props => <DefaultLayout {...props}/>} />
+                    </Switch>
+                </React.Suspense>
+            </HashRouter>
+        );
+    }
 }
 
 export default App;
---
+++ src/containers/DefaultLayout/DefaultFooter.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+    children: PropTypes.node,
+};
+
+const defaultProps = {};
+
+class DefaultFooter extends Component {
+    render() {
+        const { children, ...attributes } = this.props;
+        const current_date = new Date();
+
+        return (
+            <React.Fragment>
+                <span><a href="https://thepalladiumgroup.com">Palladium</a> &copy; {current_date.getFullYear() } KeHMIS.</span>
+                <span className="ml-auto"><span className={"fa fa-envelope"}></span>help@palladiumgroup.on.spiceworks.com &nbsp;&nbsp; <span className={"fa fa-phone"}> 0800-722440</span></span>
+            </React.Fragment>
+        );
+    }
+}
+
+DefaultFooter.propTypes = propTypes;
+DefaultFooter.defaultProps = defaultProps;
+
+export default DefaultFooter;
