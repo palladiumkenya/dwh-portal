a958e11be5473cb3af3b91264771b8bc57316431
Gitonga Kithinji <Gitonga.Kithinji@thepalladiumgroup.com>
2020-04-14 09:38:01 +0300

    Begin work on reporting rates

--- src/_nav.js
+++ src/_nav.js
@@ -1,13 +1,18 @@
 export default {
   items: [
       {
-        name: 'Dashboard',
-        url: '/dashboard',
-        icon: 'icon-speedometer',
-        badge: {
-          variant: 'info',
-        },
-    },
+          name: 'Dashboard',
+          url: '/dashboard',
+          icon: 'icon-speedometer',
+          badge: {
+              variant: 'info',
+          },
+      },
+      {
+          name: 'Reporting Rates',
+          url: '/reporting-rates',
+          icon: 'icon-pencil'
+      }
     /*{
       title: true,
       name: 'Theme',
