684bbbde975e46eed933711f10c7818dea2f345c
Gitonga Kithinji <Gitonga.Kithinji@thepalladiumgroup.com>
2020-07-06 14:41:25 +0300

    cleanup headers

--- src/views/Reporting Rates/County/ReportingRatesByCountyHeader.js
+++ src/views/Reporting Rates/County/ReportingRatesByCountyHeader.js
@@ -1,45 +1,45 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card, CardBody } from "reactstrap";
 
-class ReportingRatesByCountyHeader extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
+const ReportingRatesByCountyHeader = ({ period }) => {
+  const getYear = () => {
+      if (period) {
+          return period.split(',')[0];
+      }
+      return 2020;
+  };
+  return (
       <div className="row">
-        <div className="col-12">
-          <Card className="pageHeading_reporting">
-            <CardBody>
-              <div className="row">
-                <div className="col-5">
-                  <div className="row">
-                    <div className="col-6">
-                      <div className="reporting-rates-card-title">
-                        REPORTING RATES
+          <div className="col-12">
+              <Card className="pageHeading_reporting">
+                  <CardBody>
+                      <div className="row">
+                          <div className="col-5">
+                              <div className="row">
+                                  <div className="col-6">
+                                      <div className="reporting-rates-card-title">
+                                          REPORTING RATES
+                                      </div>
+                                  </div>
+                                  <div className="col-1">
+                                      <div className="reporting-rates-card-separator"></div>
+                                  </div>
+                                  <div className="col-5">
+                                      <div className="reporting-rates-card-overview">
+                                          BY COUNTY
+                                      </div>
+                                  </div>
+                              </div>
+                          </div>
+                          <div className="col-7">
+                              <div className="reporting-rates-card-year">Year { getYear() }</div>
+                          </div>
                       </div>
-                    </div>
-                    <div className="col-1">
-                      <div className="reporting-rates-card-separator"></div>
-                    </div>
-                    <div className="col-5">
-                      <div className="reporting-rates-card-overview">
-                        BY COUNTY
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="col-7">
-                  <div className="reporting-rates-card-year">Year 2020</div>
-                </div>
-              </div>
-            </CardBody>
-          </Card>
-        </div>
+                  </CardBody>
+              </Card>
+          </div>
       </div>
-    );
-  }
-}
+  );
+};
 
 export default ReportingRatesByCountyHeader;
