e42cc1460a4acac88dff4d2db26182e22215cb27
Eric Ejimba <ejimba@gmail.com>
2020-09-02 09:07:08 +0300

    add tx new by age and sex

--- src/views/CareAndTreatment/NewOnArt/NewOnArt.js
+++ src/views/CareAndTreatment/NewOnArt/NewOnArt.js
@@ -3,6 +3,7 @@ import HtsUptakeFilter from './../../Shared/HtsUptakeFilter';
 import NewOnArtHeader from './NewOnArtHeader';
 import NewOnArtFooter from './NewOnArtFooter';
 import NewOnArtByMonth from './NewOnArtByMonth';
+import NewOnArtByAgeSex from './NewOnArtByAgeSex';
 
 const NewOnArt = ({ period }) => {
     return (
@@ -11,6 +12,8 @@ const NewOnArt = ({ period }) => {
             <NewOnArtHeader></NewOnArtHeader>
             <HtsUptakeFilter>&nbsp;</HtsUptakeFilter>
             <p></p><NewOnArtByMonth/>
+            <hr/><NewOnArtFooter/><hr/><div className="strip"></div><p></p>
+            <p></p><NewOnArtByAgeSex/>
             <hr/><NewOnArtFooter/><hr/><div className="strip"></div><p></p>
         </div>
     );
