cc4fe95f6ec3979d501e3b25185978d756125377
Gitonga Kithinji <gitonga.kithinji@thepalladiumgroup.com>
2020-09-07 09:04:29 +0300

    Active ART implementation for adults, children and active overall
    Signed-off-by: Gitonga Kithinji <Gitonga.Kithinji@thepalladiumgroup.com>

--- src/views/CT/Home/UptakeByEntryPoint.js
+++ src/views/CT/Home/UptakeByEntryPoint.js
@@ -2,13 +2,98 @@ import React, { useEffect, useState } from 'react';
 import { Card, CardBody, CardHeader } from 'reactstrap';
 import HighchartsReact from 'highcharts-react-official';
 import Highcharts from 'highcharts';
+import AdultMan from '../../../assets/custom/AdultMan.png';
+import AdultWoman from '../../../assets/custom/AdultWoman.png';
+import { getAll } from '../../Shared/Api';
 
 const UptakeByEntryPoint = ({ globalFilter }) => {
     const [uptakeByEntryPoint, setUptakeByEntryPoint] = useState({});
+    const [ARTClients, setARTClients] = useState({
+        activeARTClients: ''
+    });
+    const [ARTClientsChildren, setARTClientsChildren] = useState({
+        ActiveARTChildren: ''
+    });
+    const [ARTClientsAdults, setARTClientsAdults] = useState({
+        ActiveARTAdults: ''
+    });
+    const [ARTClientsAdolescents, setARTClientsAdolescents] = useState({
+        ActiveARTAdolescents: ''
+    });
 
     useEffect(() => {
         loadUptakeByEntryPoint();
+        loadActiveOnART();
+        loadActiveOnARTChildren();
+        loadActiveOnARTAdults();
+        loadActiveOnARTAdolescents();
     }, [globalFilter]);
+
+    const loadActiveOnART = async () => {
+        let params = null;
+
+        if (globalFilter) {
+            params = { ...globalFilter };
+        }
+
+        let ActiveART = 0;
+
+        const result = await getAll('care-treatment/activeArt', params);
+        if(result && result.length > 0) {
+            ActiveART = result[0].ActiveART;
+        }
+
+        setARTClients({
+            activeARTClients: ActiveART.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+        });
+    };
+
+    const loadActiveOnARTChildren = async () => {
+        let params = null;
+
+        if (globalFilter) {
+            params = { ...globalFilter };
+        }
+
+        let ActiveARTChildren = 0;
+
+        const result = await getAll('care-treatment/activeArtChildren', params);
+        if(result && result.length > 0) {
+            ActiveARTChildren = result[0].ActiveARTChildren;
+        }
+
+        setARTClientsChildren({
+            ActiveARTChildren: ActiveARTChildren.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+        });
+    };
+
+    const loadActiveOnARTAdults = async () => {
+        let params = null;
+
+        if (globalFilter) {
+            params = { ...globalFilter };
+        }
+
+        let ActiveARTAdults = 0;
+
+        const result = await getAll('care-treatment/activeArtAdults', params);
+        if(result && result.length > 0) {
+            ActiveARTAdults = result[0].ActiveARTAdults;
+        }
+
+        setARTClientsAdults({
+            ActiveARTAdults: ActiveARTAdults.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+        });
+    }
+
+    const loadActiveOnARTAdolescents = async () => {
+        let params = null;
+
+        if (globalFilter) {
+            params = { ...globalFilter };
+        }
+
+    };
 
     const loadUptakeByEntryPoint = async () => {
         setUptakeByEntryPoint({
@@ -86,6 +171,71 @@ const UptakeByEntryPoint = ({ globalFilter }) => {
                         </div>
                     </CardBody>
                 </Card>
+            </div>
+            <div className="col-6">
+                <div className="hmis_statistics_separator">&nbsp;</div>
+
+                <div className="row active_art">
+                    <div className="col-12 active_art_number">
+                        <strong>{ARTClients.activeARTClients}</strong>
+                    </div>
+                    <div className="col-12">
+                        ACTIVE ON ART CLIENTS
+                    </div>
+                </div>
+
+                <div className="hmis_statistics_separator">&nbsp;</div>
+
+                <div className="row active_art">
+                    <div className="col-6 active_art_number">
+                        <strong>{ARTClientsChildren.ActiveARTChildren}</strong>
+                    </div>
+                    <div className="col-6 active_art_number">
+                        <strong>{ARTClientsAdults.ActiveARTAdults}</strong>
+                    </div>
+                    <div className="col-6">
+                        CHILDREN(0-14 YEARS)
+                    </div>
+                    <div className="col-6">
+                        ADULTS(+15 YEARS)
+                    </div>
+                </div>
+
+                <div className="hmis_statistics_separator">&nbsp;</div>
+
+                <div className="row active_art">
+                    <div className="col-12 active_art_number">
+                        <strong>{ARTClientsAdolescents.ActiveARTAdolescents}</strong>
+                    </div>
+                    <div className="col-12">
+                        ADOLESCENTS(10 -19 YEARS)
+                    </div>
+                </div>
+
+                <div className="hmis_statistics_separator">&nbsp;</div>
+
+                <div className="row active_art">
+                    <div className="col-6">
+                        <div className="row">
+                            <div className="col-1">
+                                <img src={AdultMan} width={"auto"} height={"50"}  alt={"Adult Man"} />
+                            </div>
+                            <div className="col-11 active_art_gender">
+                                237,194 males are active on ART. 93% of the males with a viral load in the last 12 months are virally suppressed.
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-6">
+                        <div className="row">
+                            <div className="col-1">
+                                <img src={AdultWoman} width={"auto"} height={"50"}  alt={"Adult Woman"} />
+                            </div>
+                            <div className="col-11 active_art_gender">
+                                475,248 females are active on ART. 94% of the females with a viral load in the last 12 months are virally suppressed.
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
