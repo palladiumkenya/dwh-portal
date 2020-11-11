bc56a766814f5ba757dd480895bf9c2bc8a5cc1a
Gitonga Kithinji <gitonga.kithinji@thepalladiumgroup.com>
2020-11-11 13:06:20 +0300

    adverse events implementation: 1. ADVERSE EVENTS ACTIONS BY SEVERITY
    Signed-off-by: Gitonga Kithinji <Gitonga.Kithinji@thepalladiumgroup.com>

--- src/views/CT/AdverseEvents/AdverseEventsSeverity.js
+++ src/views/CT/AdverseEvents/AdverseEventsSeverity.js
@@ -110,6 +110,58 @@ const AdverseEventsSeverity = ({ globalFilter }) => {
             params = { ...globalFilter };
         }
 
+        const result = await getAll('care-treatment/getAeActionsBySeverity', params);
+
+        const undocumented = result.filter(obj => obj.AdverseEventActionTaken === 'Undocumented');
+        const mild_undocumented = undocumented.length > 0 ? undocumented.filter(obj => obj.Severity === 'Mild') : [];
+        const moderate_undocumented = undocumented.length > 0 ? undocumented.filter(obj => obj.Severity === 'Moderate') : [];
+        const severe_undocumented = undocumented.length > 0 ? undocumented.filter(obj => obj.Severity === 'Severe') : [];
+        const unknown_undocumented = undocumented.length > 0 ? undocumented.filter(obj => obj.Severity === 'Unknown') : [];
+        const mild_undocumented_total = mild_undocumented.length > 0 ? mild_undocumented.map(item => item.total).reduce((i, j) => i + j) : 0;
+        const moderate_undocumented_total = moderate_undocumented.length > 0 ? moderate_undocumented.map(item => item.total).reduce((j, k) => j + k) : 0;
+        const severe_undocumented_total = severe_undocumented.length > 0 ? severe_undocumented.map(item => item.total).reduce((m, n) => m + n) : 0;
+        const unknown_undocumented_total = unknown_undocumented.length > 0 ? unknown_undocumented.map(item => item.total).reduce((x, y) => x + y) : 0;
+
+        const medicineNotChanged = result.filter(obj => obj.AdverseEventActionTaken === 'Medicine not changed');
+        const mild_medicineNotChanged = medicineNotChanged.length > 0 ? medicineNotChanged.filter(obj => obj.Severity === 'Mild') : [];
+        const moderate_medicineNotChanged = medicineNotChanged.length > 0 ? medicineNotChanged.filter(obj => obj.Severity === 'Moderate') : [];
+        const severe_medicineNotChanged = medicineNotChanged.length > 0 ? medicineNotChanged.filter(obj => obj.Severity === 'Severe') : [];
+        const unknown_medicineNotChanged = medicineNotChanged.length > 0 ? medicineNotChanged.filter(obj => obj.Severity === 'Unknown') : [];
+        const mild_medicineNotChanged_total = mild_medicineNotChanged.length > 0 ? mild_medicineNotChanged.map(item => item.total).reduce((i, j) => i + j) : 0;
+        const moderate_medicineNotChanged_total = moderate_medicineNotChanged.length > 0 ? moderate_medicineNotChanged.map(item => item.total).reduce((j, k) => j + k) : 0;
+        const severe_medicineNotChanged_total = severe_medicineNotChanged.length > 0 ? severe_medicineNotChanged.map(item => item.total).reduce((m, n) => m + n) : 0;
+        const unknown_medicineNotChanged_total = unknown_medicineNotChanged.length > 0 ? unknown_medicineNotChanged.map(item => item.total).reduce((x, y) => x + y) : 0;
+
+        const doseReduced = result.filter(obj => obj.AdverseEventActionTaken === 'Dose reduced');
+        const mild_doseReduced = doseReduced.length > 0 ? doseReduced.filter(obj => obj.Severity === 'Mild') : [];
+        const moderate_doseReduced = doseReduced.length > 0 ? doseReduced.filter(obj => obj.Severity === 'Moderate') : [];
+        const severe_doseReduced = doseReduced.length > 0 ? doseReduced.filter(obj => obj.Severity === 'Severe') : [];
+        const unknown_doseReduced = doseReduced.length > 0 ? doseReduced.filter(obj => obj.Severity === 'Unknown') : [];
+        const mild_doseReduced_total = mild_doseReduced.length > 0 ? mild_doseReduced.map(item => item.total).reduce((i, j) => i + j) : 0;
+        const moderate_doseReduced_total = moderate_doseReduced.length > 0 ? moderate_doseReduced.map(item => item.total).reduce((j, k) => j + k) : 0;
+        const severe_doseReduced_total = severe_doseReduced.length > 0 ? severe_doseReduced.map(item => item.total).reduce((m, n) => m + n) : 0;
+        const unknown_doseReduced_total = unknown_doseReduced.length > 0 ? unknown_doseReduced.map(item => item.total).reduce((x, y) => x + y) : 0;
+
+        const medicineWithdrawn = result.filter(obj => obj.AdverseEventActionTaken === 'Medicine causing AE substituted/withdrawn');
+        const mild_medicine_withdrawn = medicineWithdrawn.length > 0 ? medicineWithdrawn.filter(obj => obj.Severity === 'Mild') : [];
+        const moderate_medicine_withdrawn = medicineWithdrawn.length > 0 ? medicineWithdrawn.filter(obj => obj.Severity === 'Moderate') : [];
+        const severe_medicine_withdrawn = medicineWithdrawn.length > 0 ? medicineWithdrawn.filter(obj => obj.Severity === 'Severe') : [];
+        const unknown_medicine_withdrawn = medicineWithdrawn.length > 0 ? medicineWithdrawn.filter(obj => obj.Severity === 'Unknown') : [];
+        const mild_medicine_withdrawn_total = mild_medicine_withdrawn.length > 0 ? mild_medicine_withdrawn.map(item => item.total).reduce((i, j) => i + j) : 0;
+        const moderate_medicine_withdrawn_total = moderate_medicine_withdrawn.length > 0 ? moderate_medicine_withdrawn.map(item => item.total).reduce((j, k) => j + k) : 0;
+        const severe_medicine_withdrawn_total = severe_medicine_withdrawn.length > 0 ? severe_medicine_withdrawn.map(item => item.total).reduce((m, n) => m + n) : 0;
+        const unknown_medicine_withdrawn_total = unknown_medicine_withdrawn.length > 0 ? unknown_medicine_withdrawn.map(item => item.total).reduce((x, y) => x + y) : 0;
+
+        const allDrugsStopped = result.filter(obj => obj.AdverseEventActionTaken === 'All drugs stopped');
+        const mild_allDrugsStopped = allDrugsStopped.length > 0 ? allDrugsStopped.filter(obj => obj.Severity === 'Mild') : [];
+        const moderate_allDrugsStopped = allDrugsStopped.length > 0 ? allDrugsStopped.filter(obj => obj.Severity === 'Moderate') : [];
+        const severe_allDrugsStopped = allDrugsStopped.length > 0 ? allDrugsStopped.filter(obj => obj.Severity === 'Severe') : [];
+        const unknown_allDrugsStopped = allDrugsStopped.length > 0 ? allDrugsStopped.filter(obj => obj.Severity === 'Unknown') : [];
+        const mild_all_drugs_stopped_total = mild_allDrugsStopped.length > 0 ? mild_allDrugsStopped.map(item => item.total).reduce((i, j) => i + j) : 0;
+        const moderate_all_drugs_stopped_total = moderate_allDrugsStopped.length > 0 ? moderate_allDrugsStopped.map(item => item.total).reduce((j, k) => j + k) : 0;
+        const severe_all_drugs_stopped_total = severe_allDrugsStopped.length > 0 ? severe_allDrugsStopped.map(item => item.total).reduce((m, n) => m + n) : 0;
+        const unknown_all_drugs_stopped_total = unknown_allDrugsStopped.length > 0 ? unknown_allDrugsStopped.map(item => item.total).reduce((x, y) => x + y) : 0;
+
         setAdverseEventsActionsBySeverity({
             chart: {
                 type: 'column'
@@ -118,7 +170,7 @@ const AdverseEventsSeverity = ({ globalFilter }) => {
                 text: ''
             },
             xAxis: {
-                categories: ['MILD', 'MODERATE', 'SEVERE']
+                categories: ['MILD', 'MODERATE', 'SEVERE', 'UNKNOWN']
             },
             yAxis: {
                 min: 0,
@@ -163,23 +215,23 @@ const AdverseEventsSeverity = ({ globalFilter }) => {
             series: [{
                 name: 'Undocumented',
                 color: "#1AB394",
-                data: [5, 3, 4, 7]
+                data: [mild_undocumented_total, moderate_undocumented_total, severe_undocumented_total, unknown_undocumented_total]
             }, {
                 name: 'Medicine not changed',
                 color: "#485969",
-                data: [2, 2, 3, 2]
+                data: [mild_medicineNotChanged_total, moderate_medicineNotChanged_total, severe_medicineNotChanged_total, unknown_medicineNotChanged_total]
             }, {
                 name: 'Dose reduced',
                 color: "#D4FF78",
-                data: [2, 2, 3, 2]
+                data: [mild_doseReduced_total, moderate_doseReduced_total, severe_doseReduced_total, unknown_doseReduced_total]
             }, {
                 name: 'Medicine causing AE withdrawn',
                 color: "#1f77b4",
-                data: [2, 2, 3, 2]
+                data: [mild_medicine_withdrawn_total, moderate_medicine_withdrawn_total, severe_medicine_withdrawn_total, unknown_medicine_withdrawn_total]
             }, {
                 name: 'All drugs stopped',
                 color: "#f28e2b",
-                data: [2, 2, 3, 2]
+                data: [mild_all_drugs_stopped_total, moderate_all_drugs_stopped_total, severe_all_drugs_stopped_total, unknown_all_drugs_stopped_total]
             }]
         });
     }, [globalFilter]);
