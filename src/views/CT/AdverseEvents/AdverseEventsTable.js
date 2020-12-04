7a44036166e5b54d354223e774ceca109df4c33a
Eric Ejimba <ejimba@gmail.com>
2020-12-04 08:52:00 +0300

    add pagination to tables

--- src/views/CT/AdverseEvents/AdverseEventsTable.js
+++ src/views/CT/AdverseEvents/AdverseEventsTable.js
@@ -86,7 +86,7 @@ const AdverseEventsTable = () => {
                     </CardHeader>
                     <CardBody className="trends-body">
                         <div className="col-12">
-                            <DataTable columns={adverseEvents.columns} data={adverseEvents.data}/>
+                            <DataTable columns={adverseEvents.columns} data={adverseEvents.data} pagination="true" defaultSortField="aeName" responsive="true"/>
                         </div>
                     </CardBody>
                 </Card>
