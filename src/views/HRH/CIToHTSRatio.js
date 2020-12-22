0abdb46fd0e9c33365f2817f328fd46e139a88a6
Eric Ejimba <ejimba@gmail.com>
2020-12-22 08:11:42 +0300

    update HRH charts to current style and theme

--- src/views/HRH/CIToHTSRatio.js
+++ src/views/HRH/CIToHTSRatio.js
@@ -4,71 +4,21 @@ import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 import { getAll } from '../Shared/HRHApi';
 
-const DistributionHTSCI = ({ globalFilter }) => {
-    const [chartData, setChart] = useState({
-        chart: { type: "column" },
-        title: { text: "", style: { display: "none" } },
-        xAxis: { categories: [], title: { text: null } },
-        yAxis: { min: 0, title: { text: "", align: "high" }, labels: { overflow: "justify" } },
-        plotOptions: { bar: { dataLabels: { enabled: true } } },
-        legend: { enabled: false },
-        credits: { enabled: true },
-        responsive: { rules: [ { condition: { maxWidth: 500, }, chartOptions: { legend: { enabled: false } } } ] },
-        series: [ { data: [], color: "#1AB394" } ]
-    });
+const DistributionHTSCI = () => {
+    const [chartData, setChart] = useState({});
 
     const loadChart = useCallback(async () => {
         const data = await getAll('/getHTSDensity/');
         setChart({
-            chart: {
-                zoomType: 'xy'
-            },
-            title: {
-                text: 'Case identification to HTS providers\' ratio by County'
-            },
-            subtitle: {
-                text: 'Source: NASCOP HRIS'
-            },
-            xAxis: [{
-                categories: data.counties,
-                crosshair: true
-            }],
-            yAxis: [{ // Primary yAxis
-                labels: {
-                    format: ' {value}',
-                    style: {
-                        color: Highcharts.getOptions().colors[1]
-                    }
-                },
-                title: {
-                    text: 'CI to HTS Providers Ratio',
-                    style: {
-                        color: Highcharts.getOptions().colors[1]
-                    }
-                }
-            }],
-            tooltip: {
-                shared: true
-            },
-            legend: {
-                layout: 'vertical',
-                align: 'left',
-                x: 120,
-                verticalAlign: 'top',
-                y: 100,
-                floating: true,
-                backgroundColor:
-                    Highcharts.defaultOptions.legend.backgroundColor || // theme
-                    'rgba(255,255,255,0.25)'
-            },
-            series: [{
-                name: 'CI to HTS Providers Ratio',
-                type: 'spline',
-                data: data.ratio,
-                tooltip: {
-                    valueSuffix: ''
-                }
-            }]
+            title: { text: 'Case identification to HTS providers\' ratio by County' },
+            subtitle: { text: 'Source: NASCOP HRIS' },
+            xAxis: [{ categories: data.counties, crosshair: true }],
+            yAxis: [{ title: { text: 'CI to HTS Providers Ratio' }}],
+            tooltip: { shared: true },
+            legend: { align: 'left', verticalAlign: 'top', y: 0, x: 80, floating: true, borderWidth: 1 },
+            series: [
+                { name: 'CI to HTS Providers Ratio', type: 'spline', data: data.ratio, color: "#E06F07" }
+            ]
         });
     }, []);
     
@@ -77,18 +27,11 @@ const DistributionHTSCI = ({ globalFilter }) => {
     }, [loadChart]);
 
     return (
-        <div className="row">
-            <div className="col-12">
-                <legend>Case identification to HTS providers' ratio by County</legend>
-                <Card className="trends-card">
-                    <CardBody className="trends-body">
-                        <div className="col-12">
-                            <HighchartsReact highcharts={Highcharts} options={chartData} />
-                        </div>
-                    </CardBody>
-                </Card>
-            </div>
-        </div>
+        <Card>
+            <CardBody>
+                <HighchartsReact highcharts={Highcharts} options={chartData} />
+            </CardBody>
+        </Card>
     );
 };
 
--- src/views/HRH/DistributionHTSCI.js
+++ src/views/HRH/DistributionHTSCI.js
@@ -4,85 +4,27 @@ import Highcharts from "highcharts";
 import HighchartsReact from "highcharts-react-official";
 import { getAll } from '../Shared/HRHApi';
 
-const DistributionHTSCI = ({ globalFilter }) => {
-    const [chartData, setChart] = useState({
-        chart: { type: "column" },
-        title: { text: "", style: { display: "none" } },
-        xAxis: { categories: [], title: { text: null } },
-        yAxis: { min: 0, title: { text: "", align: "high" }, labels: { overflow: "justify" } },
-        plotOptions: { bar: { dataLabels: { enabled: true } } },
-        legend: { enabled: false },
-        credits: { enabled: true },
-        responsive: { rules: [ { condition: { maxWidth: 500, }, chartOptions: { legend: { enabled: false } } } ] },
-        series: [ { data: [], color: "#1AB394" } ]
-    });
+const DistributionHTSCI = () => {
+    const [chartData, setChart] = useState({});
 
     const loadChart = useCallback(async () => {
         const data = await getAll('/getHTSDensityScatter/');
         setChart({
-            chart: {
-                type: 'scatter',
-                zoomType: 'xy'
-            },
-            title: {
-                text: 'Distribution of HTS providers and case identification, 2019'
-            },
-            subtitle: {
-                text: 'Source: NASCOP HRIS'
-            },
-            xAxis: {
-                title: {
-                    enabled: true,
-                    text: 'No of HTS providers'
-                },
-                startOnTick: true,
-                endOnTick: true,
-                showLastLabel: true
-            },
-            yAxis: {
-                title: {
-                    text: 'No of cases identified'
-                }
-            },
-            legend: {
-                layout: 'vertical',
-                align: 'left',
-                verticalAlign: 'top',
-                x: 100,
-                y: 70,
-                floating: true,
-                backgroundColor: Highcharts.defaultOptions.chart.backgroundColor,
-                borderWidth: 1
-            },
+            title: { text: 'Distribution of HTS providers and case identification, 2019' },
+            subtitle: { text: 'Source: NASCOP HRIS' },
+            xAxis: { title: { text: 'No of HTS providers' }, startOnTick: true, endOnTick: true, showLastLabel: true },
+            yAxis: { title: { text: 'No of cases identified' }},
+            legend: { align: 'left', verticalAlign: 'top', y: 0, x: 80, floating: true, borderWidth: 1 },
             plotOptions: {
                 scatter: {
-                    marker: {
-                        radius: 5,
-                        states: {
-                            hover: {
-                                enabled: true,
-                                lineColor: 'rgb(100,100,100)'
-                            }
-                        }
-                    },
-                    states: {
-                        hover: {
-                            marker: {
-                                enabled: false
-                            }
-                        }
-                    },
-                    tooltip: {
-                        headerFormat: '<b>{series.name}</b><br>',
-                        pointFormat: '{point.x} Cases, {point.y} HTS Providers'
-                    }
+                    marker: { radius: 5, states: { hover: { enabled: true, lineColor: 'rgb(100,100,100)' }}},
+                    states: { hover: { marker: { enabled: false }}},
+                    tooltip: { headerFormat: '<b>{series.name}</b><br>', pointFormat: '{point.x} Cases, {point.y} HTS Providers' }
                 }
             },
-            series: [{
-                name: 'Cases & HTS Providers Distribution',
-                color: 'rgba(223, 83, 83, .5)',
-                data: data.count
-            }]
+            series: [
+                { name: 'Cases & HTS Providers Distribution', color: 'rgba(223, 83, 83, .5)', data: data.count, type: 'scatter' }
+            ]
         });
     }, []);
 
@@ -91,18 +33,11 @@ const DistributionHTSCI = ({ globalFilter }) => {
     }, [loadChart]);
     
     return (
-        <div className="row">
-            <div className="col-12">
-                <legend>Distribution of HTS providers and case identification, 2019</legend>
-                <Card className="trends-card">
-                    <CardBody className="trends-body">
-                        <div className="col-12">
-                            <HighchartsReact highcharts={Highcharts} options={chartData} />
-                        </div>
-                    </CardBody>
-                </Card>
-            </div>
-        </div>
+        <Card>
+            <CardBody>
+                <HighchartsReact highcharts={Highcharts} options={chartData} />
+            </CardBody>
+        </Card>
     );
 };
 
