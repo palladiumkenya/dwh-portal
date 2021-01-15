ebf1735a2757c7af006cd8377356568ca044c3fa
Eric Ejimba <ejimba@gmail.com>
2021-01-15 03:47:16 +0300

    fix hts uptake zeros

--- src/views/HTS/Uptake/UptakeTypeAndSelfTest.js
+++ src/views/HTS/Uptake/UptakeTypeAndSelfTest.js
@@ -19,7 +19,7 @@ const UptakeTypeAndSelfTest = () => {
             partner: filters.partners,
             agency: filters.agencies,
             project: filters.projects,
-            year: filters.fromDate ? moment(filters.fromDate, "MMM YYYY").format("YYYY"): moment().format("YYYY")
+            year: filters.fromDate ? moment(filters.fromDate, "MMM YYYY").format("YYYY") : ''
         };
         params.month = filters.fromDate ? moment(filters.fromDate, "MMM YYYY").format("MM") : '';
         let couple = null;
@@ -57,7 +57,7 @@ const UptakeTypeAndSelfTest = () => {
             partner: filters.partners,
             agency: filters.agencies,
             project: filters.projects,
-            year: filters.fromDate ? moment(filters.fromDate, "MMM YYYY").format("YYYY"): moment().format("YYYY")
+            year: filters.fromDate ? moment(filters.fromDate, "MMM YYYY").format("YYYY") : ''
         };
         params.month = filters.fromDate ? moment(filters.fromDate, "MMM YYYY").format("MM") : '';
         let yes = null;
