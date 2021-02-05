167ed6ec44c0e708f954ce21218757a1ebb3848d
Eric Ejimba <ejimba@gmail.com>
2021-02-05 16:17:33 +0300

    use new cards in art opt

--- src/views/CT/ArtOptimization/AdultArtOptimizationOverview.js
+++ src/views/CT/ArtOptimization/AdultArtOptimizationOverview.js
@@ -1,105 +1,62 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Row, Col, Card, CardBody, CardHeader } from 'reactstrap';
+import { Row, Col } from 'reactstrap';
+import { formatNumber, roundNumber } from '../../../utils/utils';
 import * as artOptimizationOverviewSelectors from '../../../selectors/CT/ArtOptimization/artOptimizationOverview';
+import DataCard from '../../Shared/DataCard';
 
 const AdultArtOptimizationOverview = () => {
     const adults = useSelector(artOptimizationOverviewSelectors.getAdults);
     const adultsOnFirstLine = useSelector(artOptimizationOverviewSelectors.getAdultsOnFirstLine);
-    const adultsOnFirstLinePercent = adults ? ((adultsOnFirstLine/adults)*100).toFixed(1) : 0;
+    const adultsOnFirstLinePercent = adults ? (adultsOnFirstLine/adults)*100 : 0;
     const adultsOnSecondLine = useSelector(artOptimizationOverviewSelectors.getAdultsOnSecondLine);
-    const adultsOnSecondLinePercent = adults ? ((adultsOnSecondLine/adults)*100).toFixed(1) : 0;
+    const adultsOnSecondLinePercent = adults ? (adultsOnSecondLine/adults)*100 : 0;
     const adultsOnThirdLine = useSelector(artOptimizationOverviewSelectors.getAdultsOnThirdLine);
-    const adultsOnThirdLinePercent = adults ? ((adultsOnThirdLine/adults)*100).toFixed(1) : 0;
+    const adultsOnThirdLinePercent = adults ? (adultsOnThirdLine/adults)*100 : 0;
     const adultsOnTld = useSelector(artOptimizationOverviewSelectors.getAdultsOnTld);
-    const adultsOnTldPercent = adults ? ((adultsOnTld/adults)*100).toFixed(1) : 0;
+    const adultsOnTldPercent = adults ? (adultsOnTld/adults)*100 : 0;
     const adultsOnNvp = useSelector(artOptimizationOverviewSelectors.getAdultsOnNvp);
-    const adultsOnNvpPercent = adults ? ((adultsOnNvp/adults)*100).toFixed(1) : 0;
+    const adultsOnNvpPercent = adults ? (adultsOnNvp/adults)*100 : 0;
 
     return (
         <>
             <Row>
                 <Col>
-                    <Card className="card-uploads-consistency-rates">
-                        <CardHeader className="expected-uploads-header">
-                            ADULTS ON FIRST LINE
-                        </CardHeader>
-                        <CardBody
-                            className="align-items-center d-flex justify-content-center"
-                            style={{ textAlign: 'center', backgroundColor: '#F6F6F6', height: '100px' }}
-                        >
-                            <div className="col-12">
-                                <span className="expected-uploads-text">{adultsOnFirstLine.toLocaleString('en')}</span>
-                                <sup className="overall-rates-sup"> {adultsOnFirstLinePercent.toLocaleString('en')}<span className="overall-rates-sup-perc"> %</span></sup>
-                            </div>
-                        </CardBody>
-                    </Card>
+                    <DataCard
+                        title="ADULTS ON FIRST LINE"
+                        subtitle={roundNumber(adultsOnFirstLinePercent) + "%"}
+                        data={formatNumber(adultsOnFirstLine)}
+                    />
                 </Col>
                 <Col>
-                    <Card className="card-uploads-consistency-rates">
-                        <CardHeader className="expected-uploads-header">
-                            ADULTS ON SECOND LINE
-                        </CardHeader>
-                        <CardBody
-                            className="align-items-center d-flex justify-content-center"
-                            style={{ textAlign: 'center', backgroundColor: '#F6F6F6', height: '100px' }}
-                        >
-                            <div className="col-12">
-                                <span className="expected-uploads-text">{adultsOnSecondLine.toLocaleString('en')}</span>
-                                <sup className="overall-rates-sup"> {adultsOnSecondLinePercent.toLocaleString('en')}<span className="overall-rates-sup-perc"> %</span></sup>
-                            </div>
-                        </CardBody>
-                    </Card>
+                    <DataCard
+                        title="ADULTS ON SECOND LINE"
+                        subtitle={roundNumber(adultsOnSecondLinePercent) + "%"}
+                        data={formatNumber(adultsOnSecondLine)}
+                    />
                 </Col>
                 <Col>
-                    <Card className="card-uploads-consistency-rates">
-                        <CardHeader className="expected-uploads-header">
-                            ADULTS ON THIRD LINE
-                        </CardHeader>
-                        <CardBody
-                            className="align-items-center d-flex justify-content-center"
-                            style={{ textAlign: 'center', backgroundColor: '#F6F6F6', height: '100px' }}
-                        >
-                            <div className="col-12">
-                                <span className="expected-uploads-text">{adultsOnThirdLine.toLocaleString('en')}</span>
-                                <sup className="overall-rates-sup"> {adultsOnThirdLinePercent.toLocaleString('en')}<span className="overall-rates-sup-perc"> %</span></sup>
-                            </div>
-                        </CardBody>
-                    </Card>
+                    <DataCard
+                        title="ADULTS ON THIRD LINE"
+                        subtitle={roundNumber(adultsOnThirdLinePercent) + "%"}
+                        data={formatNumber(adultsOnThirdLine)}
+                    />
                 </Col>
             </Row>
             <Row>
                 <Col>
-                    <Card className="card-uploads-consistency-rates">
-                        <CardHeader className="expected-uploads-header">
-                            ADULTS ON TLD
-                        </CardHeader>
-                        <CardBody
-                            className="align-items-center d-flex justify-content-center"
-                            style={{ textAlign: 'center', backgroundColor: '#F6F6F6', height: '100px' }}
-                        >
-                            <div className="col-12">
-                                <span className="expected-uploads-text">{adultsOnTld.toLocaleString('en')}</span>
-                                <sup className="overall-rates-sup"> {adultsOnTldPercent.toLocaleString('en')}<span className="overall-rates-sup-perc"> %</span></sup>
-                            </div>
-                        </CardBody>
-                    </Card>
+                    <DataCard
+                        title="ADULTS ON TLD"
+                        subtitle={roundNumber(adultsOnTldPercent) + "%"}
+                        data={formatNumber(adultsOnTld)}
+                    />
                 </Col>
                 <Col>
-                    <Card className="card-uploads-consistency-rates">
-                        <CardHeader className="expected-uploads-header">
-                            ADULTS ON NVP
-                        </CardHeader>
-                        <CardBody
-                            className="align-items-center d-flex justify-content-center"
-                            style={{ textAlign: 'center', backgroundColor: '#F6F6F6', height: '100px' }}
-                        >
-                            <div className="col-12" style={{ textAlign: 'center' }}>
-                                <span className="overall-rates-figure">{adultsOnNvp.toLocaleString('en')}</span>
-                                <sup className="overall-rates-sup"> {adultsOnNvpPercent.toLocaleString('en')}<span className="overall-rates-sup-perc"> %</span></sup>
-                            </div>
-                        </CardBody>
-                    </Card>
+                    <DataCard
+                        title="ADULTS ON NVP"
+                        subtitle={roundNumber(adultsOnNvpPercent) + "%"}
+                        data={formatNumber(adultsOnNvp)}
+                    />
                 </Col>
             </Row>
         </>
--- src/views/CT/ArtOptimization/ChildArtOptimizationOverview.js
+++ src/views/CT/ArtOptimization/ChildArtOptimizationOverview.js
@@ -1,70 +1,43 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Row, Col, Card, CardBody, CardHeader } from 'reactstrap';
+import { Row, Col } from 'reactstrap';
+import { formatNumber, roundNumber } from '../../../utils/utils';
 import * as artOptimizationOverviewSelectors from '../../../selectors/CT/ArtOptimization/artOptimizationOverview';
+import DataCard from '../../Shared/DataCard';
 
 const ChildArtOptimizationOverview = () => {
     const children = useSelector(artOptimizationOverviewSelectors.getChildren);
     const childrenOnFirstLine = useSelector(artOptimizationOverviewSelectors.getChildrenOnFirstLine);
-    const childrenOnFirstLinePercent = children ? ((childrenOnFirstLine/children)*100).toFixed(1) : 0;
+    const childrenOnFirstLinePercent = children ? (childrenOnFirstLine/children)*100 : 0;
     const childrenOnSecondLine = useSelector(artOptimizationOverviewSelectors.getChildrenOnSecondLine);
-    const childrenOnSecondLinePercent = children ? ((childrenOnSecondLine/children)*100).toFixed(1) : 0;
+    const childrenOnSecondLinePercent = children ? (childrenOnSecondLine/children)*100 : 0;
     const childrenOnThirdLine = useSelector(artOptimizationOverviewSelectors.getChildrenOnThirdLine);
-    const childrenOnThirdLinePercent = children ? ((childrenOnThirdLine/children)*100).toFixed(1) : 0;
+    const childrenOnThirdLinePercent = children ? (childrenOnThirdLine/children)*100 : 0;
 
     return (
-        <>
-            <Row>
-                <Col>
-                    <Card className="card-uploads-consistency-rates">
-                        <CardHeader className="expected-uploads-header">
-                            CALHIV ON FIRST LINE
-                        </CardHeader>
-                        <CardBody
-                            className="align-items-center d-flex justify-content-center"
-                            style={{ textAlign: 'center', backgroundColor: '#F6F6F6', height: '100px' }}
-                        >
-                            <div className="col-12">
-                                <span className="expected-uploads-text">{childrenOnFirstLine.toLocaleString('en')}</span>
-                                <sup className="overall-rates-sup"> {childrenOnFirstLinePercent.toLocaleString('en')}<span className="overall-rates-sup-perc"> %</span></sup>
-                            </div>
-                        </CardBody>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card className="card-uploads-consistency-rates">
-                        <CardHeader className="expected-uploads-header">
-                            CALHIV ON SECOND LINE
-                        </CardHeader>
-                        <CardBody
-                            className="align-items-center d-flex justify-content-center"
-                            style={{ textAlign: 'center', backgroundColor: '#F6F6F6', height: '100px' }}
-                        >
-                            <div className="col-12">
-                                <span className="expected-uploads-text">{childrenOnSecondLine.toLocaleString('en')}</span>
-                                <sup className="overall-rates-sup"> {childrenOnSecondLinePercent.toLocaleString('en')}<span className="overall-rates-sup-perc"> %</span></sup>
-                            </div>
-                        </CardBody>
-                    </Card>
-                </Col>
-                <Col>
-                    <Card className="card-uploads-consistency-rates">
-                        <CardHeader className="expected-uploads-header">
-                            CALHIV ON THIRD LINE
-                        </CardHeader>
-                        <CardBody
-                            className="align-items-center d-flex justify-content-center"
-                            style={{ textAlign: 'center', backgroundColor: '#F6F6F6', height: '100px' }}
-                        >
-                            <div className="col-12">
-                                <span className="expected-uploads-text">{childrenOnThirdLine.toLocaleString('en')}</span>
-                                <sup className="overall-rates-sup"> {childrenOnThirdLinePercent.toLocaleString('en')}<span className="overall-rates-sup-perc"> %</span></sup>
-                            </div>
-                        </CardBody>
-                    </Card>
-                </Col>
-            </Row>
-        </>
+        <Row>
+            <Col>
+                <DataCard
+                    title="CALHIV ON FIRST LINE"
+                    subtitle={roundNumber(childrenOnFirstLinePercent) + "%"}
+                    data={formatNumber(childrenOnFirstLine)}
+                />
+            </Col>
+            <Col>
+                <DataCard
+                    title="CALHIV ON SECOND LINE"
+                    subtitle={roundNumber(childrenOnSecondLinePercent) + "%"}
+                    data={formatNumber(childrenOnSecondLine)}
+                />
+            </Col>
+            <Col>
+                <DataCard
+                    title="CALHIV ON THIRD LINE"
+                    subtitle={roundNumber(childrenOnThirdLinePercent) + "%"}
+                    data={formatNumber(childrenOnThirdLine)}
+                />
+            </Col>
+        </Row>
     );
 };
 
