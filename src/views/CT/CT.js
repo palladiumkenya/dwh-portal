0fed576181ac9f72d625b6942a19110f2d92d28f
Eric Ejimba <ejimba@gmail.com>
2021-02-10 02:23:25 +0300

    rename current on art folder

--- src/views/CT/CT.js
+++ src/views/CT/CT.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Nav, NavItem, NavLink, TabContent, TabPane } from 'reactstrap';
 import NewOnArt from './NewOnArt/NewOnArt';
-import CurrentOnArt from './CurrentOnArt1/CurrentOnArt';
+import CurrentOnArt from './CurrentOnArt/CurrentOnArt';
 import DSD from './DSD/DSD';
 import TreatmentOutcomes from './TreatmentOutcomes/TreatmentOutcomes';
 import VL from './VL/VL';
