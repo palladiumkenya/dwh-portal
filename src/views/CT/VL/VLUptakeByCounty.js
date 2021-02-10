dd1920f6f1308644a0a9e60f9fe9a7d71d2e60d2
Eric Ejimba <ejimba@gmail.com>
2021-02-10 11:06:34 +0300

    change vl uptake to percentages

--- src/views/CT/VL/VLUptakeByCounty.js
+++ src/views/CT/VL/VLUptakeByCounty.js
@@ -28,28 +28,20 @@ const VLUptakeByCounty = () => {
 
         for(let i = 0; i < result.length; i++) {
             counties.push(result[i].county);
-            vlUptakeByCounty.push(parseInt(result[i].vlDone, 10));
+            vlUptakeByCounty.push({
+                y: Number(((parseInt(result[i].vlDone)/parseInt(result[i].txCurr))*100).toFixed(0)),
+                absoluteY: result[i].vlDone.toLocaleString('en'),
+            });
         }
 
         setVLUptakeByCounty({
-            chart: { zoomType: 'xy' },
-            title: { useHTML: true, text: ' &nbsp;', align: 'left' },
-            subtitle: { text: ' ', align: 'left' },
-            xAxis: [{ categories: counties, crosshair: true, title: { text: 'County' } }],
-            yAxis: [
-                {
-                    title: { text: 'Number of Patients', style: { color: Highcharts.getOptions().colors[1] } },
-                    labels: { format: '{value}', style: { color: Highcharts.getOptions().colors[1] } },
-                    min: 0,
-                }
-            ],
-            plotOptions: { column: { dataLabels: { enabled: true, crop: false, overflow: 'none' } } },
-            legend: {
-                floating: true, layout: 'vertical', align: 'left', verticalAlign: 'top', y: 0, x: 80,
-                backgroundColor: Highcharts.defaultOptions.legend.backgroundColor || 'rgba(255,255,255,0.25)'
-            },
+            title: { text: '' },
+            xAxis: [{ categories: counties, title: { text: 'County' }, crosshair: true }],
+            yAxis: [{ title: { text: 'Percentage of Patients' }, labels: { format: '{value} %' }}],
+            plotOptions: { column: { dataLabels: { enabled: true, crop: false, overflow: 'none', format: '{y} %' } } },
+            legend: { align: 'left', verticalAlign: 'top', y: 0, x: 80 },
             series: [
-                { name: 'Number of Patients', data: vlUptakeByCounty, type: 'column', color: "#485969" },
+                { name: 'Percentage of Patients', data: vlUptakeByCounty, type: 'column', color: "#485969", tooltip: { valueSuffix: ' % ({point.absoluteY})'} },
             ]
         });
     }, [filters]);
