a957339d8ecadab6973441be7f0248f520ff0f73
Eric Ejimba <ejimba@gmail.com>
2021-02-16 17:15:45 +0300

    fixes inverted mmd

--- src/views/CT/DSD/AppointmentDurationByStability.js
+++ src/views/CT/DSD/AppointmentDurationByStability.js
@@ -18,8 +18,8 @@ const AppointmentDurationByStability = () => {
             plotOptions: { column: { stacking: 'percent' } },
             legend: { align: 'left', verticalAlign: 'top', y: 0, x: 80 },
             series: [
-                { name: 'NON MMD', data: appointmentDurationByStabilityStatus.data[1], type: 'column', color: "#485969", tooltip: { valueSuffix: ' ({point.percentage:.0f}%)' } },
-                { name: 'MMD', data: appointmentDurationByStabilityStatus.data[0], type: 'column', color: "#1AB394", tooltip: { valueSuffix: ' ({point.percentage:.0f}%)' } },
+                { name: 'MMD', data: appointmentDurationByStabilityStatus.data[1], type: 'column', color: "#1AB394", tooltip: { valueSuffix: ' ({point.percentage:.0f}%)' } },
+                { name: 'NON MMD', data: appointmentDurationByStabilityStatus.data[0], type: 'column', color: "#485969", tooltip: { valueSuffix: ' ({point.percentage:.0f}%)' } },
             ]
         });
     }, [appointmentDurationByStabilityStatus]);
