09c68414faa60dfc00740c87df84963e24436ea7
Gitonga Kithinji <Gitonga.Kithinji@thepalladiumgroup.com>
2021-03-08 12:42:33 +0300

    update dsd DistributionStableByPartner.js and DistributionStableByCounty.js tooltips and DistributionStableAgeSex.js tooltip with actual numbers
    Signed-off-by: Gitonga Kithinji <Gitonga.Kithinji@thepalladiumgroup.com>

--- src/selectors/CT/Dsd/dsdStabilityStatusByCounty.js
+++ src/selectors/CT/Dsd/dsdStabilityStatusByCounty.js
@@ -20,7 +20,7 @@ export const getStabilityStatusByCounty = createSelector(
             counties.push(list[i].county.toUpperCase());
             stability.push({
                 y: Math.round(list[i].percentStable*100),
-                text: list[i].patients
+                text:  '<b>Stable Patients: ' + list[i].stablePatients + ' </b><br/> <b>Txcurr:' + list[i].patients + '</b>'
             });
         }
         return { counties, stability };
