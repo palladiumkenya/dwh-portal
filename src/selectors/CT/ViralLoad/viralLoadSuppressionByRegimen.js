731babd9b97fb013fe8cf01b8ad60f1502e0ab89
Gitonga Kithinji <Gitonga.Kithinji@thepalladiumgroup.com>
2021-03-22 16:23:23 +0300

    VL OUTCOMES AMONG CURRENT ON ART PATIENTS BY ART REGIMEN/OPTIMIZATION refactor
    Signed-off-by: Gitonga Kithinji <Gitonga.Kithinji@thepalladiumgroup.com>

--- src/selectors/CT/ViralLoad/viralLoadSuppressionByRegimen.js
+++ src/selectors/CT/ViralLoad/viralLoadSuppressionByRegimen.js
@@ -2,37 +2,26 @@ import { createSelector } from 'reselect';
 
 const listUnfiltered = state => state.viralLoadSuppressionByRegimen.listUnfiltered;
 const listFiltered = state => state.viralLoadSuppressionByRegimen.listFiltered;
-const listUnfilteredCurrentOnArtOverview = state => state.currentOnArtOverview.listUnfiltered;
-const listFilteredCurrentOnArtOverview = state => state.currentOnArtOverview.listFiltered;
 const filtered = state => state.filters.filtered;
 
 export const getViralLoadSuppressionByRegimen = createSelector(
-    [listUnfiltered, listFiltered, listUnfilteredCurrentOnArtOverview, listFilteredCurrentOnArtOverview, filtered],
-    (listUnfiltered, listFiltered, listUnfilteredCurrentOnArtOverview, listFilteredCurrentOnArtOverview, filtered) => {
+    [listUnfiltered, listFiltered, filtered],
+    (listUnfiltered, listFiltered, filtered) => {
         const list = filtered ? listFiltered : listUnfiltered;
-        const listCurrentOnArtOverview = filtered ? listFilteredCurrentOnArtOverview : listUnfilteredCurrentOnArtOverview;
-        const hasCurrentVl = listCurrentOnArtOverview.Last12MonthVL ? listCurrentOnArtOverview.Last12MonthVL : 0;
-        const regimenCategories = ['TLD', 'TLE', 'OTHERS'];
+        const regimenCategories = ['TLD', 'TLE', 'NVP', 'Other Regimen'];
+        const retentionCategories = ['VL', 'HVL', 'LVL'];
         let data = [];
-        for(let i = 0; i < regimenCategories.length; i++) {
-            data[i] = 0;
-        }
-        if (hasCurrentVl > 0) {
-            for(let i = 0; i < list.length; i++) {
-                let regimenIndex = regimenCategories.indexOf(list[i].regimen);
-                if(regimenIndex === -1) {
-                    if(list[i].regimen === 'Other Regimen') {
-                        regimenIndex = 2;
-                    } else {
-                        continue;
-                    }
+        for (const retentionCategory of retentionCategories) {
+            const categoryData = [];
+            for (const regimenCategory of regimenCategories) {
+                const filteredValues = list.filter(obj => obj.Last12MVLResult === retentionCategory && obj.regimen === regimenCategory);
+                if (filteredValues.length > 0) {
+                    categoryData.push(filteredValues[0].count);
+                } else {
+                    categoryData.push(0);
                 }
-                data[regimenIndex] = {
-                    y: Number(((parseInt(list[i].txCurr)/parseInt(hasCurrentVl))*100).toFixed(1)),
-                    absoluteY: list[i].txCurr.toLocaleString('en'),
-                    text: Number(((parseInt(list[i].txCurr)/parseInt(hasCurrentVl))*100).toFixed(1)) + '%',
-                };
             }
+            data.push(categoryData);
         }
 
         return { regimenCategories, data };
