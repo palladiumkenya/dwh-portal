6ad0b1108eacddac793df86ae209eeb6ac5a7997
Eric Ejimba <ejimba@gmail.com>
2021-04-28 12:27:34 +0300

    added loading icons

--- src/reducers/RR/overallReportingRatesByFacilityNotReported.js
+++ src/reducers/RR/overallReportingRatesByFacilityNotReported.js
@@ -26,20 +26,23 @@ const initialState = {
 export default (state = initialState, action) => {
     let newState = { ...state };
     switch (action.type) {
-        case actionTypes.RR_OVERALL_REPORTING_RATES_BY_FACILITY_REQUEST:
+        case actionTypes.RR_OVERALL_REPORTING_RATES_BY_FACILITY_NOT_REPORTED_REQUEST:
             newState.loading[action.payload.docket] = true;
+            newState.lastFetch[action.payload.docket] = Date.now();
             return newState;
-        case actionTypes.RR_OVERALL_REPORTING_RATES_BY_FACILITY_FETCH:
+        case actionTypes.RR_OVERALL_REPORTING_RATES_BY_FACILITY_NOT_REPORTED_FETCH:
             if (action.payload.filtered === true) {
                 newState.listFiltered[action.payload.docket] = action.payload.list;
             } else {
                 newState.listUnfiltered[action.payload.docket] = action.payload.list;
-                newState.lastFetch[action.payload.docket] = Date.now();
             }
             newState.loading[action.payload.docket] = false;
+            newState.lastFetch[action.payload.docket] = Date.now();
+            console.log('RR_OVERALL_REPORTING_RATES_BY_FACILITY_NOT_REPORTED_FETCH');
             return newState;
-        case actionTypes.RR_OVERALL_REPORTING_RATES_BY_FACILITY_REQUEST_FAILED:
+        case actionTypes.RR_OVERALL_REPORTING_RATES_BY_FACILITY_NOT_REPORTED_REQUEST_FAILED:
             newState.loading[action.payload.docket] = false;
+            newState.lastFetch[action.payload.docket] = null;
             return newState;
         default:
             return state
