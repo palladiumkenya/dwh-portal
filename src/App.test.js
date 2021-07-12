9f8ce42b44bdf2c75476f976e61f6507d24250c3
Felix Kithinji Gitonga <kithinjifelix@gmail.com>
2021-07-12 17:24:09 +0300

    Implemnted complted TLD chart

--- src/App.test.js
+++ src/App.test.js
@@ -6,4 +6,6 @@ test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+
+
 });
