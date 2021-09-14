3233a724ec3a3fd7c1f9505ea6a5bb9c4b21b281
Felix Kithinji Gitonga <kithinjifelix@gmail.com>
2021-09-14 10:09:09 +0300

    add ADULT PLHIV VACCINATED AGAINST COVID-19 BY AGE data

--- src/views/CT/Covid/COVIDAdultPLHIVVaccinatedByAge.js
+++ src/views/CT/Covid/COVIDAdultPLHIVVaccinatedByAge.js
@@ -6,6 +6,25 @@ import Highcharts from 'highcharts';
 const COVIDAdultPLHIVVaccinatedByAge = () => {
     const [covidVaccinatedByAge, setCovidVaccinatedByAge] = useState({});
 
+    const loadVaccinatedByAge = useCallback(async () => {
+        setCovidVaccinatedByAge({
+            title: { text: '' },
+            plotOptions: { column: { stacking: 'percent' } },
+            xAxis: [{ categories: ['18-25 YRS', '26-35 YRS', '36-45 YRS', '46-55 YRS', '56-65 YRS', '>=66 YRS'], crosshair: true }],
+            yAxis: [{ title: { text: 'Percentage of Patients' }}],
+            tooltip: { shared: true },
+            legend: { align: 'left', reversed: true, verticalAlign: 'top', y: 0, x: 80 },
+            series: [
+                { name: 'PARTIALLY VACCINATED', data: [45, 80, 30, 25, 66, 22], type: 'column', color: "#F08532", tooltip: { valueSuffix: ' ({point.percentage:.0f}%)' } },
+                { name: 'FULLY VACCINATED', data: [55, 20, 78, 90, 21, 22], type: 'column', color: "#69B34C", tooltip: { valueSuffix: ' ({point.percentage:.0f}%)' } },
+            ]
+        });
+    }, []);
+
+    useEffect(() => {
+        loadVaccinatedByAge();
+    }, []);
+
     return (
         <Card className="trends-card">
             <CardHeader className="trends-header" style={{textTransform: 'none'}}>
