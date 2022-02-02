b28498800172d5fe72a7eb895a1e3d00bc737987
cbrianbet <cbrianbet@outlook.com>
2022-02-02 23:29:53 +0300

    donut metrics

--- src/views/CT/Covid/COVIDAdultPlhivCurrentOnTreatment.js
+++ src/views/CT/Covid/COVIDAdultPlhivCurrentOnTreatment.js
@@ -1,22 +1,72 @@
 import React, { useCallback, useEffect, useState } from 'react';
 import moment from 'moment';
+import HighchartsReact from 'highcharts-react-official';
+import Highcharts from 'highcharts';
 
-import * as covidAdultPLHIVCurrentOnTreatmentSelectors from '../../../selectors/CT/Covid/covidAdultPLHIVCurrentOnTreatment';
+import * as covidAdultPLHIVCurrentOnTreatmentSelectors
+    from '../../../selectors/CT/Covid/covidAdultPLHIVCurrentOnTreatment';
 import { useSelector } from 'react-redux';
 import { formatNumber } from '../../../utils/utils';
 import DataCard from '../../Shared/DataCard';
+import { Card, CardBody, CardFooter } from 'reactstrap';
 
 const COVIDAdultPlhivCurrentOnTreatment = () => {
+    const [covidAdultPlhivCurrentOnTreatment, setCovidAdultPlhivCurrentOnTreatment] = useState({});
 
     const currentOnArtAdults = useSelector(covidAdultPLHIVCurrentOnTreatmentSelectors.getAdultPLHIVCurrentOnTreatment).covidAdultsPLHIVCurrentOnTreatment;
-    const adultPLHIVCurrentOnTreatment = "ADULT >15 YEARS PLHIV CURRENT ON TREATMENT as at " + moment().startOf('month').subtract(1, 'month').format('MMM YYYY');
+
+    const label = 'ADULT >15 YEARS PLHIV CURRENT ON TREATMENT';
+
+    const data = [{
+        y: currentOnArtAdults * 100 / currentOnArtAdults,
+        color: '#063970'
+    }, {
+        y: 100 - currentOnArtAdults,
+        color: 'rgba(0,0,0,0)'
+    }];
+
+    let title = `<p> </p><br><p style="font-size: 35px;font-weight: bold; line-height: 2">${currentOnArtAdults.toLocaleString()}</p><br><p>AS AT ${moment().startOf('month').subtract(1, 'month').format('MMM YYYY')}</p>`;
+    const loadCovidAdultPlhivCurrentOnTreatment = useCallback(async () => {
+        setCovidAdultPlhivCurrentOnTreatment({
+            chart: {
+                renderTo: 'container',
+                type: 'pie'
+            },
+            title: {
+                text: title,
+                align: 'center',
+                verticalAlign: 'middle',
+                y: 0
+            },
+            plotOptions: {
+                pie: {
+                    innerSize: 300,
+                    dataLabels: false
+                }
+            },
+            series: [{
+                data: data
+            }],
+            credits: {
+                enabled: false
+            },
+            tooltip: { enabled: false },
+            exporting: {
+                enabled: false
+            }
+        });
+    }, [currentOnArtAdults]);
+
+    useEffect(() => {
+        loadCovidAdultPlhivCurrentOnTreatment();
+    }, [loadCovidAdultPlhivCurrentOnTreatment]);
+
 
     return (
-        <DataCard
-            title={adultPLHIVCurrentOnTreatment}
-            subtitle={null}
-            data={formatNumber(currentOnArtAdults)}
-        />
+        <div>
+            <HighchartsReact highcharts={Highcharts} options={covidAdultPlhivCurrentOnTreatment}/>
+            <p>{label}</p>
+        </div>
     );
 };
 
--- src/views/CT/Covid/COVIDNumberScreened.js
+++ src/views/CT/Covid/COVIDNumberScreened.js
@@ -6,20 +6,67 @@ import * as covidNumberScreenedSelectors from '../../../selectors/CT/Covid/covid
 import * as covidAdultPLHIVCurrentOnTreatmentSelectors
     from '../../../selectors/CT/Covid/covidAdultPLHIVCurrentOnTreatment';
 import { useSelector } from 'react-redux';
+import moment from 'moment';
+import HighchartsReact from 'highcharts-react-official';
+import Highcharts from 'highcharts';
 
 const COVIDNumberScreened = () => {
+    const [covidNumberScreened, setCovidNumberScreened] = useState({});
     const currentOnArtAdults = useSelector(covidAdultPLHIVCurrentOnTreatmentSelectors.getAdultPLHIVCurrentOnTreatment).covidAdultsPLHIVCurrentOnTreatment;
-    const screened = useSelector(covidNumberScreenedSelectors.getCovidNumberScreened).Screened;
-
-    let percentScreened = screened && Number(screened) > 0 ? ((Number(screened)/Number(currentOnArtAdults))*100) : 0;
+    const screened = useSelector(covidNumberScreenedSelectors.getCovidNumberScreened).Screened || 0;
+    let percentScreened = screened && Number(screened) > 0 ? ((Number(screened) / Number(currentOnArtAdults)) * 100) : 0;
     percentScreened = Math.round((percentScreened + Number.EPSILON) * 100) / 100;
 
+    const data = [{
+        y: percentScreened,
+        color: '#e88134'
+    }, {
+        y: 100 - percentScreened,
+        color: 'rgba(0,0,0,0)'
+    }];
+
+    let label = 'SCREENED FOR VACCINATION'
+
+    let title = `<p style="font-size: 15px">${percentScreened} %</p><br><p style="font-size: 35px;font-weight: bold">${screened.toLocaleString()}</p><br><p>AS AT ${moment().startOf('month').subtract(1, 'month').format('MMM YYYY')}</p>`;
+    const loadCovidNumberScreened = useCallback(async () => {
+        setCovidNumberScreened({
+            chart: {
+                renderTo: 'container',
+                type: 'pie'
+            },
+            title: {
+                text: title,
+                align: 'center',
+                verticalAlign: 'middle',
+                y: 0
+            },
+            plotOptions: {
+                pie: {
+                    innerSize: 300,
+                    dataLabels: false
+                }
+            },
+            series: [{
+                data: data
+            }],
+            credits: {
+                enabled: false
+            },
+            tooltip: { enabled: false },
+            exporting: {
+                enabled: false
+            }
+        });
+    }, [screened]);
+
+    useEffect(() => {
+        loadCovidNumberScreened();
+    }, [loadCovidNumberScreened]);
     return (
-        <DataCard
-            title="NUMBER SCREENED FOR COVID-19 VACCINATION"
-            subtitle={roundNumber(percentScreened) + "%"}
-            data={formatNumber(screened)}
-        />
+        <div>
+            <HighchartsReact highcharts={Highcharts} options={covidNumberScreened}/>
+            <p>{label}</p>
+        </div>
     );
 };
 export default COVIDNumberScreened;
