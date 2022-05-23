0c436bd71ad1fdae91f236111041335bc3af202d
cbrianbet <cbrianbet@outlook.com>
2022-05-23 16:35:49 +0300

    Removed some old drugs from AE

--- src/selectors/CT/AdverseEvents/adverseEventsProportionOfPLHIVWithAeByTypeOfSuspectedCausativeDrugs.js
+++ src/selectors/CT/AdverseEvents/adverseEventsProportionOfPLHIVWithAeByTypeOfSuspectedCausativeDrugs.js
@@ -1,12 +1,25 @@
 import { createSelector } from 'reselect';
 
-const listUnFiltered = state => state.adverseEventsProportionOfPlHivWithAeByCausativeDrugs.listUnfiltered;
-const listFiltered = state => state.adverseEventsProportionOfPlHivWithAeByCausativeDrugs.listFiltered;
-const filtered = state => state.filters.filtered;
+const listUnFiltered = (state) =>
+    state.adverseEventsProportionOfPlHivWithAeByCausativeDrugs.listUnfiltered;
+const listFiltered = (state) =>
+    state.adverseEventsProportionOfPlHivWithAeByCausativeDrugs.listFiltered;
+const filtered = (state) => state.filters.filtered;
 
-export const getProportionOfPLHIVWithAeByTypeOfSuspectedCausativeDrugs = createSelector(
-    [listUnFiltered, listFiltered, filtered],
-    (listUnFiltered, listFiltered, filtered) => {
-        return filtered ? listFiltered : listUnFiltered;
-    }
-);
+export const getProportionOfPLHIVWithAeByTypeOfSuspectedCausativeDrugs =
+    createSelector(
+        [listUnFiltered, listFiltered, filtered],
+        (listUnFiltered, listFiltered, filtered) => {
+            listFiltered = listFiltered.filter(function (item) {
+                return !['Stavudine', 'Didanosin'].includes(
+                    item.adverseEventCause
+                );
+            });
+            listUnFiltered = listUnFiltered.filter(function (item) {
+                return !['Stavudine', 'Didanosin'].includes(
+                    item.adverseEventCause
+                );
+            });
+            return filtered ? listFiltered : listUnFiltered;
+        }
+    );
