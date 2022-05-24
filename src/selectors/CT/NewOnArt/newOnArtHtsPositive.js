905534ebf646a92c123d30596c64bc60d0451627
cbrianbet <cbrianbet@outlook.com>
2022-05-24 21:15:11 +0300

    Aligned charts with previous reporting month

--- src/selectors/CT/NewOnArt/newOnArtHtsPositive.js
+++ src/selectors/CT/NewOnArt/newOnArtHtsPositive.js
@@ -1,31 +1,66 @@
 import { createSelector } from 'reselect';
 import moment from 'moment';
 
-const listUnfiltered = state => state.newOnArtTrends.listUnfiltered;
-const listFiltered = state => state.newOnArtTrends.listFiltered;
-const listUnfilteredPositiveTrends = state => state.linkagePositiveTrends.listUnfiltered;
-const listFilteredPositiveTrends = state => state.linkagePositiveTrends.listFiltered;
-const filtered = state => state.filters.filtered;
+const listUnfiltered = (state) => state.newOnArtTrends.listUnfiltered;
+const listFiltered = (state) => state.newOnArtTrends.listFiltered;
+const listUnfilteredPositiveTrends = (state) =>
+    state.linkagePositiveTrends.listUnfiltered;
+const listFilteredPositiveTrends = (state) =>
+    state.linkagePositiveTrends.listFiltered;
+const filtered = (state) => state.filters.filtered;
 
 export const getNewOnArtHtsPositive = createSelector(
-    [listUnfiltered, listFiltered, listUnfilteredPositiveTrends, listFilteredPositiveTrends, filtered],
-    (listUnfiltered, listFiltered, listUnfilteredPositiveTrends, listFilteredPositiveTrends, filtered) => {
+    [
+        listUnfiltered,
+        listFiltered,
+        listUnfilteredPositiveTrends,
+        listFilteredPositiveTrends,
+        filtered,
+    ],
+    (
+        listUnfiltered,
+        listFiltered,
+        listUnfilteredPositiveTrends,
+        listFilteredPositiveTrends,
+        filtered
+    ) => {
         const list = filtered ? listFiltered : listUnfiltered;
-        const listPositiveTrends = filtered ? listFilteredPositiveTrends : listUnfilteredPositiveTrends;
+        const listPositiveTrends = filtered
+            ? listFilteredPositiveTrends
+            : listUnfilteredPositiveTrends;
         const monthNames = {
-            1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June",
-            7: "July", 8:"August", 9: "September", 10: "October", 11: "November", 12: "December"
+            1: 'January',
+            2: 'February',
+            3: 'March',
+            4: 'April',
+            5: 'May',
+            6: 'June',
+            7: 'July',
+            8: 'August',
+            9: 'September',
+            10: 'October',
+            11: 'November',
+            12: 'December',
         };
         let months = [];
         let txNew = [];
         let positives = [];
 
-        const today = new Date();
+        const today = moment().subtract(2, 'month').add(15, 'days');
         let previousYearDate = moment(today).subtract(1, 'years').toDate();
         previousYearDate = moment(previousYearDate).add(1, 'month').toDate();
+
         while (previousYearDate <= today) {
-            months.push(monthNames[previousYearDate.getMonth() + 1] + ' ' + previousYearDate.getFullYear());
-            const filteredMonthYear = list.filter(obj => obj.month === (previousYearDate.getMonth() + 1) && obj.year === previousYearDate.getFullYear());
+            months.push(
+                monthNames[previousYearDate.getMonth() + 1] +
+                    ' ' +
+                    previousYearDate.getFullYear()
+            );
+            const filteredMonthYear = list.filter(
+                (obj) =>
+                    obj.month === previousYearDate.getMonth() + 1 &&
+                    obj.year === previousYearDate.getFullYear()
+            );
             if (filteredMonthYear.length > 0) {
                 let totalTxNew = 0;
                 for (const filteredMonthYearElement of filteredMonthYear) {
@@ -34,17 +69,25 @@ export const getNewOnArtHtsPositive = createSelector(
                 txNew.push(parseInt(totalTxNew.toString(), 10));
             }
 
-            const filteredPositives = listPositiveTrends.filter(obj => obj.month === (previousYearDate.getMonth() + 1) && obj.year === previousYearDate.getFullYear());
+            const filteredPositives = listPositiveTrends.filter(
+                (obj) =>
+                    obj.month === previousYearDate.getMonth() + 1 &&
+                    obj.year === previousYearDate.getFullYear()
+            );
             for (const filteredPositive of filteredPositives) {
-                positives.push(parseInt(filteredPositive.positive.toString(), 10));
+                positives.push(
+                    parseInt(filteredPositive.positive.toString(), 10)
+                );
             }
-            previousYearDate = moment(previousYearDate).add(1, 'month').toDate();
+            previousYearDate = moment(previousYearDate)
+                .add(1, 'month')
+                .toDate();
         }
         if (filtered) {
-            months = []
-            list.forEach(date => {
-                months.push(monthNames[date.month] + ' ' + date.year)
-            })
+            months = [];
+            list.forEach((date) => {
+                months.push(monthNames[date.month] + ' ' + date.year);
+            });
         }
         return { months, txNew, positives };
     }
--- src/selectors/CT/NewOnArt/newOnArtTrends.js
+++ src/selectors/CT/NewOnArt/newOnArtTrends.js
@@ -1,45 +1,74 @@
 import _ from 'lodash';
 import { createSelector } from 'reselect';
 
-const listUnfiltered = state => state.newOnArtTrends.listUnfiltered;
-const listFiltered = state => state.newOnArtTrends.listFiltered;
-const filtered = state => state.filters.filtered;
+const listUnfiltered = (state) => state.newOnArtTrends.listUnfiltered;
+const listFiltered = (state) => state.newOnArtTrends.listFiltered;
+const filtered = (state) => state.filters.filtered;
 
 export const getNewOnArtTrends = createSelector(
     [listUnfiltered, listFiltered, filtered],
     (listUnfiltered, listFiltered, filtered) => {
         const list = filtered ? listFiltered : listUnfiltered;
         const monthNames = {
-            1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June",
-            7: "July", 8:"August", 9: "September", 10: "October", 11: "November", 12: "December"
+            1: 'January',
+            2: 'February',
+            3: 'March',
+            4: 'April',
+            5: 'May',
+            6: 'June',
+            7: 'July',
+            8: 'August',
+            9: 'September',
+            10: 'October',
+            11: 'November',
+            12: 'December',
         };
         let months = [];
         let txNew = [];
 
-        for(let i = 0; i < list.length; i++) {
-            const selectedArr = list.filter(obj => obj.month === list[i].month && obj.year === list[i].year);
-            if (selectedArr.length > 0 && months.indexOf(monthNames[list[i].month] + ' ' + list[i].year.toString()) === -1) {
+        for (let i = 0; i < list.length; i++) {
+            const selectedArr = list.filter((obj) =>
+                obj.month === list[i].month && obj.year === list[i].year
+            );
+
+            if (
+                selectedArr.length > 0 &&
+                months.indexOf(
+                    monthNames[list[i].month] + ' ' + list[i].year.toString()
+                ) === -1
+            ) {
                 let totalTxNew = 0;
                 let femaleVal = 0;
                 let maleVal = 0;
                 for (const selectedArrElement of selectedArr) {
-                    if (months.indexOf(monthNames[selectedArrElement.month] + ' ' + selectedArrElement.year.toString()) === -1) {
-                        months.push(monthNames[selectedArrElement.month] + ' ' + selectedArrElement.year.toString());
+                    if (
+                        months.indexOf(
+                            monthNames[selectedArrElement.month] + ' ' + selectedArrElement.year.toString()
+                        ) === -1
+                    ) {
+                        months.push(
+                            monthNames[selectedArrElement.month] +
+                                ' ' +
+                                selectedArrElement.year.toString()
+                        );
                     }
                     totalTxNew = totalTxNew + parseInt(selectedArrElement.txNew, 10);
-                    if (selectedArrElement.gender === "Female") {
-                        femaleVal = (parseInt(selectedArrElement.txNew, 10));
+                    if (selectedArrElement.gender === 'Female') {
+                        femaleVal = parseInt(selectedArrElement.txNew, 10);
                     }
 
-                    if (selectedArrElement.gender === "Male") {
-                        maleVal  = (parseInt(selectedArrElement.txNew, 10));
+                    if (selectedArrElement.gender === 'Male') {
+                        maleVal = parseInt(selectedArrElement.txNew, 10);
                     }
                 }
-                txNew.push(
-                    {
-                        y: totalTxNew,
-                        text: "<b>Female: </b>" + femaleVal + ", <b>Male: </b>" + maleVal
-                    });
+                txNew.push({
+                    y: totalTxNew,
+                    text:
+                        '<b>Female: </b>' +
+                        femaleVal +
+                        ', <b>Male: </b>' +
+                        maleVal,
+                });
             }
         }
 
@@ -55,36 +84,68 @@ export const getNewOnArtLastYear = createSelector(
     (listUnfiltered, listFiltered, filtered) => {
         const list = filtered ? listFiltered : listUnfiltered;
         const monthNames = {
-            1: "January", 2: "February", 3: "March", 4: "April", 5: "May", 6: "June",
-            7: "July", 8:"August", 9: "September", 10: "October", 11: "November", 12: "December"
+            1: 'January',
+            2: 'February',
+            3: 'March',
+            4: 'April',
+            5: 'May',
+            6: 'June',
+            7: 'July',
+            8: 'August',
+            9: 'September',
+            10: 'October',
+            11: 'November',
+            12: 'December',
         };
         let months = [];
         let txNew = [];
 
-        for(let i = 0; i < list.length; i++) {
-            const selectedArr = list.filter(obj => obj.month === list[i].month && obj.year === list[i].year);
-            if (selectedArr.length > 0 && months.indexOf(monthNames[list[i].month] + ' ' + list[i].year.toString()) === -1) {
+        for (let i = 0; i < list.length; i++) {
+            const selectedArr = list.filter(
+                (obj) =>
+                    obj.month === list[i].month && obj.year === list[i].year
+            );
+            if (
+                selectedArr.length > 0 &&
+                months.indexOf(
+                    monthNames[list[i].month] + ' ' + list[i].year.toString()
+                ) === -1
+            ) {
                 let totalTxNew = 0;
                 let femaleVal = 0;
                 let maleVal = 0;
                 for (const selectedArrElement of selectedArr) {
-                    if (months.indexOf(monthNames[selectedArrElement.month] + ' ' + selectedArrElement.year.toString()) === -1) {
-                        months.push(monthNames[selectedArrElement.month] + ' ' + selectedArrElement.year.toString());
+                    if (
+                        months.indexOf(
+                            monthNames[selectedArrElement.month] +
+                                ' ' +
+                                selectedArrElement.year.toString()
+                        ) === -1
+                    ) {
+                        months.push(
+                            monthNames[selectedArrElement.month] +
+                                ' ' +
+                                selectedArrElement.year.toString()
+                        );
                     }
-                    totalTxNew = totalTxNew + parseInt(selectedArrElement.txNew, 10);
-                    if (selectedArrElement.gender === "Female") {
-                        femaleVal = (parseInt(selectedArrElement.txNew, 10));
+                    totalTxNew =
+                        totalTxNew + parseInt(selectedArrElement.txNew, 10);
+                    if (selectedArrElement.gender === 'Female') {
+                        femaleVal = parseInt(selectedArrElement.txNew, 10);
                     }
 
-                    if (selectedArrElement.gender === "Male") {
-                        maleVal  = (parseInt(selectedArrElement.txNew, 10));
+                    if (selectedArrElement.gender === 'Male') {
+                        maleVal = parseInt(selectedArrElement.txNew, 10);
                     }
                 }
-                txNew.push(
-                    {
-                        y: totalTxNew,
-                        text: "<b>Female: </b>" + femaleVal + ", <b>Male: </b>" + maleVal
-                    });
+                txNew.push({
+                    y: totalTxNew,
+                    text:
+                        '<b>Female: </b>' +
+                        femaleVal +
+                        ', <b>Male: </b>' +
+                        maleVal,
+                });
             }
         }
 
