4814f47c4d61f399bd0928d2d14a31ee1d567d78
cbrianbet <cbrianbet@outlook.com>
2022-12-21 02:02:16 -0800

    Update ViralLoad.js

--- src/views/CT/ViralLoad/ViralLoad.js
+++ src/views/CT/ViralLoad/ViralLoad.js
@@ -93,8 +93,8 @@ const ViralLoad = () => {
                         <CardHeader>Indicator Definition</CardHeader>
                         <CardBody>
                             <ul>
-                                <li>Eligible for Viral Load =&gt; Patients who are current on treatment for more than 6 months</li>
-                                <li>Valid Viral Load =&gt; Patients who are current on treatment for more than 6 months and have a viral load result whose sample was taken within the last 14 months of the latest visit.</li>
+                                <li>Eligible for Viral Load =&gt; Patients who are current on treatment for more than 12 months</li>
+                                <li>Valid Viral Load =&gt; Patients who are current on treatment for more than 12 months and have a viral load result whose sample was taken within the last 14 months of the latest visit.</li>
                             </ul>
                         </CardBody>
                     </Card>
