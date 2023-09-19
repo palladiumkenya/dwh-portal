db7d440869c28c0ecaf374f9fd506c04238cc357
cbrianbet <cbrianbet@outlook.com>
2023-09-19 13:44:37 +0300

    Changes to service desk dashboards

--- src/selectors/ServiceDesk/partnerLevel.js
+++ src/selectors/ServiceDesk/partnerLevel.js
@@ -9,9 +9,7 @@ const filtered = state => state.filters.filtered;
 export const getPartnerLevel = createSelector(
     [listUnfiltered, listFiltered, filtered],
     (listUnfiltered, listFiltered, filtered) => {
-        const list = filtered ? listFiltered : listUnfiltered;
-
-        return list;
+        return filtered ? listFiltered : listUnfiltered;
     }
 );
 
