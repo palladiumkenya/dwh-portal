d90fcc6160d5e9cb2591be64870453d70f00aa5e
cbrianbet <cbrianbet@outlook.com>
2024-03-17 23:46:32 -0700

    Update HomeVLCascade.js

--- src/views/Home/HomeVLCascade.js
+++ src/views/Home/HomeVLCascade.js
@@ -14,7 +14,7 @@ const HomeVLCascade = () => {
     const currentDate = moment();
 
     // Check if the current date is before the 20th of the month
-    if (currentDate.date() < 20) {
+    if (currentDate.date() < 18) {
         // If true, subtract 2 months
         currentDate.subtract(2, 'months');
     } else {
