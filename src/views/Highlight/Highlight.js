df1a8dbe2b07d9879ca43715c5d80c19778d0a87
cbrianbet <cbrianbet@outlook.com>
2024-05-19 20:20:40 +0300

    Added COP dashboards to highlight of the month

--- src/views/Highlight/Highlight.js
+++ src/views/Highlight/Highlight.js
@@ -1,30 +1,9 @@
-import { useState, useEffect } from 'react';
-import { DWH_API_URL } from '../../constants';
-import axios from 'axios';
+import HighlightTabs from './HighlightTabs';
 
 const Highlight = () => {
-    const [iframeUrl, setIframeUrl] = useState('');
-
-    useEffect(() => {
-        axios.get(`${DWH_API_URL}self-service/monthly-highlight`)
-            .then(res => {
-                const url = res.data?.url;
-                setIframeUrl(url);
-            })
-            .catch(error => {
-                console.error('Error fetching iframe URL:', error);
-            });
-    }, []);
-    console.log(iframeUrl)
 
     return (
-        <iframe
-            src={iframeUrl}
-            frameBorder={0}
-            width={'100%'}
-            height={800}
-            allowTransparency
-        />
+        <HighlightTabs />
     );
 }
 
