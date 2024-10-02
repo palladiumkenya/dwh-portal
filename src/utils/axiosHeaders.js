9291e9a50fcc17603dcf9b14ec8d6eb90c079a0b
cbrianbet <cbrianbet@outlook.com>
2024-10-02 18:16:27 +0300

    updated packages

--- src/utils/axiosHeaders.js
+++ src/utils/axiosHeaders.js
@@ -1,4 +1,4 @@
-import * as axios from 'axios';
+import axios from 'axios';
 
 export function setAuthHeader(token) {
     axios.defaults.headers.common['Authorization'] = token ? 'Bearer ' + token : '';
